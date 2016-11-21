f5c35669885513f81d64d1fe2bdb8c3cf70f2b6d
Day40_git <10756494142qq.com>
2016-11-21 19:09:57 +0800

    dddsssxxx

---
+++ Quanmingzhibo/app/src/main/java/com/example/administrator/quanmingzhibo/fragment/WodeFragment.java
@@ -0,0 +1,28 @@
+package com.example.administrator.quanmingzhibo.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.administrator.quanmingzhibo.R;
+
+
+public class WodeFragment extends Fragment {
+
+    public WodeFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_wode, container, false);
+    }
+
+
+}
