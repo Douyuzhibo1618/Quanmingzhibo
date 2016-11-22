88970e2ec3939139fe95fde0bb2e5f097f95ce9e
zhaixianwei <zhaixianwei>
2016-11-22 09:39:09 +0800

    AAAAAA

--- Quanmingzhibo/app/src/main/java/com/example/administrator/quanmingzhibo/MainActivity.java
+++ Quanmingzhibo/app/src/main/java/com/example/administrator/quanmingzhibo/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //dfghjkl
+        //dfghjkldgj
         setContentView(R.layout.activity_main);
         rg= (RadioGroup) findViewById(R.id.rg);
         manager=getSupportFragmentManager();
